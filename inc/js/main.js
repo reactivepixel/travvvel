7868c08171145b5336dfcb0ecdfc7d11ec505d95
Christopher Chapman <cchapman@fullsail.com>
2012-10-28 03:23:21 -0400

    Now pulling multiple pages from Dribble popular shoots for the day

--- inc/js/main.js
+++ inc/js/main.js
@@ -1,36 +1,60 @@
 $(document).ready(function() {
+	
+
+	var t 			= new Date(),
+		day 		= t.getDate(),
+		month		= t.getMonth(),
+		year		= t.getFullYear()
+		strToday	= year + "-" + month + "-" + day;
+
+	console.log("today", strToday)
+	// Returns, e.g., 6/09/07
+	
+	loadDribbleStream();
+	
+
+});
+
+var dataStream,
+	imgIndex = 0,
+	pageIndex = 1,
+	intNext,
+	maxPages;
+
+function nextImg(){
+	imgData = dataStream.shots[imgIndex]
+	var img = $("#gallery").prepend('<img class="closed" src="' + imgData.image_url + '" />');
+	setTimeout(openImg(img), 1000);
+	imgIndex++;
+	if(imgIndex >= dataStream.shots.length){
+		imgIndex = 0;
+		pageIndex++;
+		if(pageIndex > maxPages){
+			pageIndex = 0;
+		}
+		clearInterval(intNext);
+		loadDribbleStream();
+		return false;
+	}
+}
+
+function openImg(elem){
+	$(elem).find('.closed').css('width', '300px');
+}
+function loadDribbleStream(){
 	var input;
 	$.ajax({
-		url			: "http://api.dribbble.com/shots/popular/",
+		url			: "http://api.dribbble.com/shots/popular/" + strToday + "?page=" + pageIndex,
 		dataType	: "jsonp",
 		data 		: input,
 		success		: function(d){
+			console.log('data loaded', d);
 			dataStream = d;
+			maxPages = dataStream.pages;
 			intNext = setInterval(nextImg, 1000);
 		},
 		error		: function(d){
 			console.log("failure", d);
 		}
 	})
-
-});
-
-var dataStream,
-	index = 0,
-	intNext;
-
-function nextImg(){
-	if(index >= dataStream.shots.length){
-		delete intNext;
-		return false;
-	}
-	imgData = dataStream.shots[index]
-	var img = $("#gallery").append('<img class="closed" src="' + imgData.image_url + '" />');
-	setTimeout(openImg(img), 1000);
-	index++;
-}
-
-function openImg(elem){
-	console.log(elem);
-	$(elem).find('.closed').css('width', '300px');
 }
