c8daa79921a6421e1d27e6e4af896866dd2a4d4f
Christopher Chapman <cchapman@fullsail.com>
2012-10-30 14:58:06 -0400

    adding advanced click thru info

--- inc/js/main.js
+++ inc/js/main.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-	
 
 	var t 			= new Date(),
 		day 		= t.getDate(),
@@ -12,19 +11,39 @@ $(document).ready(function() {
 	
 	loadDribbleStream();
 	
-
 });
 
 var dataStream,
-	imgIndex = 0,
-	pageIndex = 1,
-	intNext,
-	maxPages;
+	imgIndex 				= 0,
+	pageIndex 				= 1,
+	intNext					,
+	maxPages				,
+	aryImgs 				= [],
+	durration_on_img 		= 10000,
+	durration_of_transition = 1000;
 
 function nextImg(){
 	imgData = dataStream.shots[imgIndex]
-	var img = $("#gallery").prepend('<img class="closed" src="' + imgData.image_url + '" />');
-	setTimeout(openImg(img), 1000);
+	aryImgs.push(imgData);
+
+	// transition main img to invis, alter img's src and create UI for main disp, transition this all back to opaque
+	var aryInfo 	= 	[
+							{"likes" 			: imgData.likes_count },
+							{"comments" 		: imgData.comments_count },
+							{"views" 			: imgData.views_count }
+						]
+
+	var mainImg = $('#mainDisp').transition({ opacity: 0 }, 0)
+		.html('<a href="' + imgData.short_url + '"><img src="' + imgData.image_url + '" /></a>')
+		.append( aryToUL( aryInfo, "infoList" ) )
+		.transition({ opacity: 100 }, durration_of_transition);
+
+	if(aryImgs.length > 1){
+		var prevImg = aryImgs[aryImgs.length-2];
+		var img = $("#gallery").prepend('<a href="' + prevImg.short_url + '"><img class="closed" src="' + prevImg.image_url + '" />');
+	}
+	
+	setTimeout(openImg(img), durration_on_img);
 	imgIndex++;
 	if(imgIndex >= dataStream.shots.length){
 		imgIndex = 0;
@@ -41,6 +60,7 @@ function nextImg(){
 function openImg(elem){
 	$(elem).find('.closed').css('width', '300px');
 }
+
 function loadDribbleStream(){
 	var input;
 	$.ajax({
@@ -51,10 +71,28 @@ function loadDribbleStream(){
 			console.log('data loaded', d);
 			dataStream = d;
 			maxPages = dataStream.pages;
-			intNext = setInterval(nextImg, 1000);
+			nextImg();
+		//	intNext = setInterval(nextImg, durration_on_img);
 		},
 		error		: function(d){
 			console.log("failure", d);
 		}
 	})
+}
+
+// Basic Utilities
+function aryToUL(ary, className){
+	var strReturn = "<ul class='" + className + "''>";
+	for(var i = 0; i < ary.length; i++){
+		objInner = ary[i];
+		for( var key in objInner){
+			strReturn += "<li class='" + key + "'>";
+			//strReturn += "<span class='title'>" + key + "</span>";
+			strReturn += "<span class='value'>" + objInner[key] + "</span>";
+			strReturn += "</li>";
+		};
+	}
+	strReturn += "</ul>";
+	console.log("str form", strReturn);
+	return strReturn;
 }
